eb675621ffc7aab1d2ae3d961a81128eeabb2638
spdeux <sepideh_masoomi@yahoo.com>
2018-03-29 17:41:26 +0430

    fix shopping cart in wishlist page
    fix shopping cart in wishlist page

--- src/app/wish-list/wish-list.component.ts
+++ src/app/wish-list/wish-list.component.ts
@@ -3,7 +3,9 @@ import {WishListService} from "../services/wish-list.service";
 import {wishList} from "../model/wishList";
 import {Router} from "@angular/router";
 import {ProductService} from "../services/product.service";
-
+import {Product} from "../model/product";
+import {ShoppingCart} from "../model/shopping-cart";
+import {ShoppingCartService} from "../services/shopping-cart.service";
 
 @Component({
   selector: 'app-wish-list',
@@ -13,14 +15,18 @@ import {ProductService} from "../services/product.service";
 export class WishListComponent implements OnInit {
   wishlistCollection = [];
   userId: number = 1;
+  shoppingCart:ShoppingCart;
+  disabledAddToCart:boolean=false;
 
   constructor(private wishListService: WishListService,
               private productService: ProductService,
+              private shoppingCartService: ShoppingCartService,
               private router: Router) {
   }
 
   ngOnInit() {
     this.getWishListByUser(this.userId);
+    this.shoppingCart=new ShoppingCart();
   }
 
   getWishListByUser(userId:number) {
@@ -36,6 +42,38 @@ export class WishListComponent implements OnInit {
         console.log(result2);
       });
     });
+  }
+
+  AddToShoppingCart(product: Product) {
+
+    this.disabledAddToCart=true;
+    this.shoppingCart.userId = this.userId;
+    this.shoppingCart.product = product;
+    this.shoppingCart.quantity = 1;
+
+    this.shoppingCartService.IsExistProductInShoppingCart(this.userId, product.id).then(result => {
+      if (result.length == 0) {
+        this.shoppingCartService.addToShoppingCart(this.shoppingCart).then(result2 => {
+          this.shoppingCartService.addEvent.emit(1);
+          this.disabledAddToCart=false;
+          // console.log(result2);
+        });
+      }
+      else {
+        this.shoppingCartService.getShoppingCartByUserIdAndProductId(this.userId, product.id).then(result4 => {
+          this.shoppingCart.id=result4[0].id;
+          this.shoppingCart.quantity+=result4[0].quantity;
+
+          this.shoppingCartService.UpdateShoppingCart(this.shoppingCart).then(result3 => {
+            this.disabledAddToCart=false;
+            // console.log(result3);
+
+          });
+
+        });
+      }
+    });
+
   }
 
   onNavigate(product) {
